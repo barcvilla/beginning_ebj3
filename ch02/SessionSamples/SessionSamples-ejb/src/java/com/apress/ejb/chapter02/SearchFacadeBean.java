3f83bb2ffd32a10005771382ed12bb7355379b51
Carlos Villanueva <ceva_19@hotmail.com>
2017-12-13 00:17:01 -0300

    Chapter 02 - Session Beans Sample

---
+++ ch02/SessionSamples/SessionSamples-ejb/src/java/com/apress/ejb/chapter02/SearchFacadeBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apress.ejb.chapter02;
+
+import javax.ejb.Stateless;
+
+/**
+ * Sample Stateless Session Bean
+ * @author barcvilla
+ */
+@Stateless(name="SearchFacade")
+public class SearchFacadeBean 
+{
+    public SearchFacadeBean()
+    {
+    }
+    
+    
+}
