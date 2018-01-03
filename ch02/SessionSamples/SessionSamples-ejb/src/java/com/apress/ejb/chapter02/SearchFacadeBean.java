60790dfef243f0b410e37fab72547c419a3e2c87
Carlos Villanueva <ceva_19@hotmail.com>
2018-01-02 21:52:48 -0300

    Chapter 2 - Stateless Session Beans

--- ch02/SessionSamples/SessionSamples-ejb/src/java/com/apress/ejb/chapter02/SearchFacadeBean.java
+++ ch02/SessionSamples/SessionSamples-ejb/src/java/com/apress/ejb/chapter02/SearchFacadeBean.java
@@ -6,8 +6,13 @@
 package com.apress.ejb.chapter02;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.ejb.Stateless;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.InvocationContext;
 
 /**
  * Sample Stateless Session Bean
@@ -26,6 +31,8 @@ public class SearchFacadeBean implements SearchFacade, SearchFacadeLocal
     public SearchFacadeBean()
     {
     }
+    
+    HashMap countryMap = new HashMap();
 
     @Override
     public List wineSearch(String wineType) 
@@ -45,5 +52,44 @@ public class SearchFacadeBean implements SearchFacade, SearchFacadeLocal
         return wineList;
     }
     
+    /**
+     * Este CallBack ocurre despues que una instancia de un bean se crea. Por tanto, se inicializara una lista
+     * de vinos del pais cade vez que el bean sea instanciado.
+     */
+    @PostConstruct
+    public void initializeCountryWineList()
+    {
+        countryMap.put("Australia", "Sauvignon Blanc");
+        countryMap.put("Austrilia", "Grenache");
+        countryMap.put("France","Gewurztraminer");
+        countryMap.put("France","Burdeos");
+    }
     
+    @PreDestroy
+    public void destroyWineList()
+    {
+        countryMap.clear();
+    }
+    
+    // se declara un metodo interceptor
+    @AroundInvoke
+    public Object TimerLog(InvocationContext ctx) throws Exception
+    {
+        String beanClassName = ctx.getClass().getName();
+        String businessMethodName = ctx.getMethod().getName();
+        String target = beanClassName + "." + businessMethodName;
+        long startTime = System.currentTimeMillis();
+        System.out.println("Invoking: "+ target);
+        try
+        {
+            return ctx.proceed();
+        }
+        finally
+        {
+            System.out.println("Exiting: " + target);
+            long totalTime = System.currentTimeMillis() - startTime;
+            System.out.println("Business method: " + businessMethodName + "in " + beanClassName + "takes " + 
+                    totalTime + "ms to execute");
+        }
+    }
 }
