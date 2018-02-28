253a2d962259b75eafb3e39d0a7a890b15058f13
Carlos Villanueva <ceva_19@hotmail.com>
2018-02-27 21:52:05 -0300

    CallBack Lifecycle Events

--- ch02/SessionSamples/SessionSamples-ejb/src/java/com/apress/ejb/chapter02/ShoppingCartBean.java
+++ ch02/SessionSamples/SessionSamples-ejb/src/java/com/apress/ejb/chapter02/ShoppingCartBean.java
@@ -41,18 +41,33 @@ public class ShoppingCartBean implements ShoppingCart, ShoppingCartLocal
         this.cartItems = cartItems;
     }
     
+    /**
+     * este evento callback ocurre despues que la instancia de un bean ha sido instanciado en el contendor EJB
+     * Si el bean no usa ningun mecanismo de inyeccion de dependencia para adquirir referencias a recursos u otro
+     * objetos en su ambiente, el evento @PostConstruct ocurre luego que injection es llevado a cabo y antes que
+     * el primero evento de negocio en el bean es llamado.
+     */
     @PostConstruct
     public void initialized()
     {
         cartItems = new ArrayList();
     }
     
+    /**
+     * El evento callback @PreDestroy ocurre luego que cualquier metodo donde la anotacion @Remove ha sido completado
+     * En este caso ShoppingCart tenemos el metodo exit() que escribe la lista cartItems en una base de datos.
+     */
     @PreDestroy
     public void exit()
     {
         System.out.println("Saved items listo into DataBase");
     }
     
+    /**
+     * El evento callback @Remove es un evento muy util para un session bean StateFul. Cuando el metodo con la 
+     * anotacion @Remove es llamada, el contenedor removera la instancia del bean del object pool luego que el metodo
+     * es ejecutado. 
+     */
     @Remove
     public void stopSession()
     {
